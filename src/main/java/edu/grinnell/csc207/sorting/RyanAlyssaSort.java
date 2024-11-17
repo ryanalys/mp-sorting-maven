8290d542590d023b124825fce2c072382f61d6e0
Ryanalys <ryanalys@grinnell.edu>
2024-11-16 22:23:51 -0600

    Updated personal sorter file

--- src/main/java/edu/grinnell/csc207/sorting/RyanAlyssaSort.java
+++ src/main/java/edu/grinnell/csc207/sorting/RyanAlyssaSort.java
@@ -1,5 +1,7 @@
 package edu.grinnell.csc207.sorting;
 
+import java.util.Comparator;
+
 /**
  * Something that sorts 
  *
@@ -7,7 +9,52 @@ package edu.grinnell.csc207.sorting;
  *   The types of values that are sorted.
  *
  * @author Alyssa Ryan
+ * @author Sam Rebelsky
  */
-public class RyanAlyssaSort {
+public class RyanAlyssaSort <T> implements Sorter <T> {
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
 
+  /**
+   * The way in which elements are ordered.
+   */
+  Comparator<? super T> order;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Create a sorter using a particular comparator.
+   *
+   * @param comparator
+   *   The order in which elements in the array should be ordered
+   *   after sorting.
+   */
+  public RyanAlyssaSort(Comparator<? super T> comparator) {
+    this.order = comparator;
+  } // SelectionSorter(Comparator)
+
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Sort an array in place using selection sort.
+   *
+   * @param values
+   *   an array to sort.
+   *
+   * @post
+   *   The array has been sorted according to some order (often
+   *   one given to the constructor).
+   * @post
+   *   For all i, 0 &lt; i &lt; values.length,
+   *     order.compare(values[i-1], values[i]) &lt;= 0
+   */
+  @Override
+  public void sort(T[] values) {
+    
+  } //sort
 } // RyanAlyssaSorter
